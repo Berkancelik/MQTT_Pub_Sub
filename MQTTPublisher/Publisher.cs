852f31507bd56f562cdaf6cbdfb67d53b26e7553
Berkan Çelik <celikberkan1@otulook.com>
2022-08-25 14:17:29 +0300

    Publisher configuration fixed

--- MQTTPublisher/Publisher.cs
+++ MQTTPublisher/Publisher.cs
@@ -1,12 +1,58 @@
-﻿ using System;
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using System;
+using System.Threading.Tasks;
 
 namespace MQTTPublisher
 {
     internal class Publisher
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var mqttFactory = new MqttFactory();
+            var client = mqttFactory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder()
+                .WithClientId(Guid.NewGuid().ToString())
+                .WithTcpServer("test.mosquitto.org", 1883)
+                .WithCleanSession()
+                .Build();
+            client.UseConnectedHandler(e =>
+            {
+                Console.WriteLine("Connected to the broker successfull");
+            });
+
+            client.UseDisconnectedHandler(e =>
+            {
+                Console.WriteLine("Disconnected frım the broker successfully");
+            });
+
+            await client.ConnectAsync(options);
+
+            Console.WriteLine("Please press a key to publish the message");
+
+            Console.ReadLine();
+            await PublihsMessageAsync(client);
+
+            await client.DisconnectAsync();
+        }
+
+            private  static async Task PublihsMessageAsync(IMqttClient client)
+            {
+                string messagePayload = "Hello!";
+                var message = new MqttApplicationMessageBuilder()
+                    .WithTopic("Berkancelik")
+                    .WithPayload(messagePayload)
+                    .WithAtLeastOnceQoS()
+                    .Build();
+
+                if (client.IsConnected)
+                {
+                    await client.PublishAsync(message);
+                }
+                
+            }
+
         }
     }
 }
